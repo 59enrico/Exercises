e9e8a6bb44bc8f43acb0fb43aa05350d234d3a51
Leue.Enrico <Leue.Enrico@SK-D43D7EB3B8BB.2631.skool.net>
2017-09-19 15:31:25 +0200

    First commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,10 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+int a = 5000;
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		try{Thread.sleep(5000);}catch(InterruptedException e) {}; //5000 mil.sec. Pause da 1000 zu kurz 
-		System.out.println("I waited 5 seconds to say this.");
+		try{Thread.sleep(a);}catch(InterruptedException e) {}; //5000 mil.sec. Pause da 1000 zu kurz 
+		System.out.println("I waited 5000 miliseconds to say this.");
 	}
 
 }
