88ac27590e260dc3ed14d630887ab3864660ca11
59enrico <enileue@gmail.com>
2017-09-26 15:19:32 +0200

    Update primeTester.java

--- Exercises/src/primeTester.java
+++ Exercises/src/primeTester.java
@@ -3,30 +3,31 @@ public class primeTester {
 
 	public static void main(String[] args) {
 
-		int n = 2;
-		for(n <= 1000; n=n+1;)
-		//System.out.println(isPrime(1000));
+		for(int n = 3; n <=1000; n++){
+			
+			if(isPrime(n))
+			
+			System.out.println(n);
+		}
+		
 	}
-
+		
 	private static boolean isPrime(int n){
-	
-		boolean prime = true;
+		boolean prime=true;
 		int i = 2;
 	
-
-		if(n == 1){
-			prime = false;
-		}
-		else{
-			while (i <= n-1 && prime == true){
-				if (n%i==0) {
-					prime = false;
-				}
-	     
-				i = i+1;
-			}
-		}
-		return prime;
-		
+		  
+		  if (n == 1) {
+		    prime=false;
+		  }
+		  else{
+		    while (i <= n-1 && prime == true) {
+		      if (n%i==0) {
+		        prime=false;
+		      }
+		      i=i+1;
+		    }
+		  }
+		 return prime;
 	}
 }
