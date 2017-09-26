4d0dfeb5d4919be7beccf3dc7a53cd7e6330ba71
Leue.Enrico <Leue.Enrico@SK-D43D7EB3BACC.2631.skool.net>
2017-09-26 14:50:46 +0200

    First Commit

---
+++ Exercises/src/primeTester.java
@@ -0,0 +1,31 @@
+
+public class primeTester {
+
+	public static void main(String[] args) {
+
+		int n = 2;
+		for(n <= 1000; n=n+1;)
+		//System.out.println(isPrime(1000));
+	}
+
+	private static boolean isPrime(int n){
+	
+		boolean prime = true;
+		int i = 2;
+	
+
+		if(n == 1){
+			prime = false;
+		}
+		else{
+			while (i <= n-1 && prime == true){
+				if (n%i==0) {
+					prime = false;
+				}
+	     
+				i = i+1;
+			}
+		}
+		return prime;
+	}
+}
